c4c19c90e8ab8a527c4727293e9fb8e0f90d2d42
khaledelbasha22 <mr.khaled.dev@gmail.com>
2021-07-04 19:08:10 +0400

    Fix App.js

--- pages/_app.js
+++ pages/_app.js
@@ -14,33 +14,14 @@ import Layout from "../components/layout/Layout";
 import { CookiesProvider } from "react-cookie"
 
 function MyApp({ Component, pageProps }) {
-<<<<<<< HEAD
-    if(Component.name == "Login"){
-        return (
-            <CookiesProvider>
-=======
 
     return (
         <CookiesProvider>
             <Layout>
->>>>>>> d0d1032f94a1a1a88090603b660e533bf399732d
                 <Component {...pageProps} />
             </Layout>
         </CookiesProvider>
     );
 
-<<<<<<< HEAD
-        )
-    }else{
-  return (
-      <CookiesProvider>
-      <Layout>
-          <Component {...pageProps} />
-      </Layout>
-      </CookiesProvider>
-  );
-=======
->>>>>>> d0d1032f94a1a1a88090603b660e533bf399732d
-}
 }
 export default store.withRedux(MyApp);
